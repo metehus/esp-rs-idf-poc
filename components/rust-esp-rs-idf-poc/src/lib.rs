ace70d28f8b7e162306b574c3428dc70a1c54ca9
Matheus Sanches <methidezeeni@gmail.com>
2024-08-29 20:25:38 -0300

    chore: things

--- components/rust-esp-rs-idf-poc/src/lib.rs
+++ components/rust-esp-rs-idf-poc/src/lib.rs
@@ -1,3 +1,12 @@
+use anyhow::Ok;
+use esp_idf_svc::hal::spi;
+use esp_idf_svc::hal::{
+    gpio::{Input, Level, Output, Pull},
+    prelude::Peripherals,
+    spi::{Spi, SpiDriver, SpiDriverConfig},
+};
+use gc9a01::GC9A01;
+
 extern "C" {
     fn read_temp_sensor() -> f32;
 }
@@ -11,13 +20,35 @@ extern "C" fn rust_main() {
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-
     log::info!("Hello, world!");
 
-    let temperature = unsafe {
-        read_temp_sensor()
-    };
+    let temperature = unsafe { read_temp_sensor() };
 
     log::info!("temp: {}", temperature);
+
+    main();
 }
 
+
+fn main() -> anyhow::Result<()> {
+    let peripherals = Peripherals::take()?;
+
+    let sclk = peripherals.pins.gpio1;
+    let serial_in = peripherals.pins.gpio16; // SDI
+    let serial_out = peripherals.pins.gpio17; // SDO
+    let cs = peripherals.pins.gpio3;
+    let dc = peripherals.pins.gpio19;
+
+    let spi = SpiDriver::new(
+        peripherals.spi2,
+        sclk,
+        serial_out,
+        Some(serial_in),
+        &SpiDriverConfig::new(),
+    )?;
+
+    let mut display = GC9A01 { spi, cs, dc };
+    display.setup();
+
+    Ok(())
+}
